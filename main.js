9abd0b73b0620b1201746a4eb535eeee2163e601
xinshu17 <77688943+xinshu17@users.noreply.github.com>
2021-04-14 10:44:06 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,21 +4,13 @@ var menuNav = document.getElementById('menu_nav');
 
 var menuTrigger = document.getElementById('menu_trigger');
 
-var header=document.getElementById('name');
-
-var header2=document.getElementById('name2');
-
 function menuToggle() {
   if (menuNav.style.display === "flex") {
     menuNav.style.display = "none";
     menuTrigger.style.background = "none";
-    header.style.display = 'flex';
-    header2.style.display = 'none';
   } else {
     menuNav.style.display = "flex";
     menuTrigger.style.background = "#79c9a2";
-    header.style.display = 'none';
-    header2.style.display = 'flex';
   }
   console.log("The menu has been toggled!")
 }
